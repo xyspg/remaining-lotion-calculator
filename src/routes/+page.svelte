e1e1cd42e49b9dbd38a152f735c7bc23d67f4785
Kang Jiaming <kangjm2005@hotmail.com>
2023-01-19 17:19:18 +0800

    making style better

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,10 +26,17 @@
 		return result.toJSON().toString();
 	}
 </script>
-
-<body class="bg-gray-100">
+<link
+		rel="stylesheet"
+		href="https://unpkg.com/claymorphism-css/dist/clay.css"
+/>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=ZCOOL+XiaoWei&display=swap" rel="stylesheet">
+<body>
 	<div class="absolute inset-48 leading-8 opacity-95">
-		<h2 class="text-2xl text-center"><ink data-type="blurred">润滑液</ink>计算器</h2>
+	<div class="clay card">
+		<h2 class="text-4xl text-center"><ink data-type="blurred">润滑液</ink>计算器</h2>
 <!--		动态获取爱液信息-->
 		{#each aiye as flavour}
 			<label class="flex items-center mb-3 space-x-3">
@@ -44,14 +51,14 @@
 			</label>
 		{/each}
 		<p>添加自定义mL</p>
-		<input placeholder="添加自定义mL" bind:value={customize} /><br />
+		<input placeholder="添加自定义mL" class="text-gray-800 pl-1 rounded-lg" bind:value={customize} /><br />
 
 		<input type="range" bind:value={usage} min="1" max="20" />
 		单次用量：<span>{usage}mL</span><br />
 		<input type="range" bind:value={frequency} min="0.25" max="5.0" step="0.25" />
 		频率：一天<span>{frequency}</span>次<br />
 		购入日期：
-		<input type="date" bind:value={purchaseDate} />
+		<input type="date" class="text-gray-800 pl-1 rounded-lg" bind:value={purchaseDate} />
 <!--		如果没有进行输入则显示prompt，否则显示结果-->
 		{#if ((flavours <= 0) & (customize === 0)) | (customize === null)}
 			<p>选择你目前拥有的<ink data-type="blurred"> 润滑液 </ink></p>
@@ -71,13 +78,14 @@
 			</p>
 		{/if}
 	</div>
+	</div>
 </body>
 
 <style>
-	h2,
-	p,
-	label {
-		font-family: 'PingFang SC', serif;
+	body {
+		background-color: #B0CFE5;
+		font-family: 'ZCOOL XiaoWei', sans-serif;
+
 	}
 	ink[data-type='blurred'] {
 		filter: blur(4px);
@@ -89,5 +97,14 @@
 	label {
 		display: flex;
 		flex-flow: row nowrap;
+	}
+	.card{
+		/* Modify clay.css properties */
+		--clay-background: #f76d6d;
+		--clay-border-radius: 48px;
+
+		/* Extended styles */
+		color: #f1f1f1;
+		padding: 48px;
 	}
 </style>
