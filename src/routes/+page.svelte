c1fe0ff698eac913a6fc84ef3d53411d549143cc
Kang Jiaming <kangjm2005@hotmail.com>
2023-01-19 16:03:23 +0800

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,92 @@
+<script>
+	const aiye = [
+		{ id: 1, name: 'MagicEyes 180mL装', volume: 180 },
+		{ id: 2, name: 'Maccos 双子 300mL装', volume: 300 },
+		{ id: 3, name: 'G Project 红瓶 220mL装', volume: 220 }
+	];
+	let flavours = [];
+	let usage = 5;
+	let frequency = 1;
+	let purchaseDate;
+	let customize = 0;
+	function join(flavours) {
+		let sum = 0;
+		flavours.forEach((item) => {
+			sum += item;
+		});
+		return sum;
+	}
+
+	function addDays(date, days) {
+		let result = new Date(date);
+		result.setDate(result.getDate() + days);
+		return result.toJSON().toString();
+	}
+
+</script>
+
+<body class="bg-gray-100">
+	<div class="absolute inset-48 leading-8 opacity-95">
+		<h2 class="text-2xl text-center"><ink data-type="blurred">润滑液</ink>计算器</h2>
+		{#each aiye as flavour}
+			<label class="flex items-center mb-3 space-x-3">
+				<input
+					class="form-tick appearance-none bg-white bg-check h-6 w-6 border border-gray-300 rounded-md checked:bg-blue-500 checked:border-transparent focus:outline-none mr-4"
+					type="checkbox"
+					bind:group={flavours}
+					name="flavours"
+					value={flavour.volume}
+				/>
+				{flavour.name}
+			</label>
+		{/each}
+		<p>添加自定义mL</p>
+		<input
+				placeholder="添加自定义mL"
+				bind:value={customize}
+		><br>
+		<input type="range" bind:value={usage} min="1" max="20" />
+		单次用量：<span>{usage}mL</span><br />
+		<input type="range" bind:value={frequency} min="0.25" max="5.0" step="0.25" />
+		频率：一天<span>{frequency}</span>次<br />
+		购入日期：
+		<input type="date" bind:value={purchaseDate} />
+
+		{#if flavours <= 0 & customize === 0 | customize === null}
+			<p>选择你目前拥有的<ink data-type="blurred"> 润滑液 </ink></p>
+		{:else}
+			<p>
+				目前总量：{parseInt(join(flavours))+parseInt(customize)} mL<br />
+				预计在冲 {Math.ceil((parseInt(join(flavours))+parseInt(customize)) / usage)} 次后耗尽<br />
+				{#if purchaseDate != null}
+					耗尽日期：<span
+						>{addDays(purchaseDate, Math.ceil((parseInt(join(flavours))+parseInt(customize)) / usage) / frequency).slice(
+							0,
+							10
+						)}</span
+					>
+				{/if}
+			</p>
+		{/if}
+	</div>
+</body>
+
+<style>
+	h1,
+	h2,
+	p,
+	label {
+		font-family: 'PingFang SC', serif;
+	}
+	ink[data-type='blurred'] {
+		filter: blur(4px);
+	}
+
+	ink[data-type='blurred']:hover {
+		filter: blur(0px);
+	}
+	label {
+		display: flex;
+		flex-flow: row nowrap;
+	}
+</style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["checked"],
+      borderColor: ["checked"],
+      inset: ["checked"],
+      zIndex: ["hover", "active"],
+    },
+  },
+  plugins: [],
+}
+
