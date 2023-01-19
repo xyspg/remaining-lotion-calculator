855358160313a6788a21763a0ca3f9b4fb5a6f80
Kang Jiaming <kangjm2005@hotmail.com>
2023-01-19 16:57:34 +0800

    add comment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,17 @@
 <script>
+	//爱液种类
 	const aiye = [
 		{ id: 1, name: 'MagicEyes 180mL装', volume: 180 },
 		{ id: 2, name: 'Maccos 双子 300mL装', volume: 300 },
 		{ id: 3, name: 'G Project 红瓶 220mL装', volume: 220 }
 	];
+	//定义常量
 	let flavours = [];
 	let usage = 5;
 	let frequency = 1;
 	let purchaseDate;
 	let customize = 0;
+	//计算总容量
 	function join(flavours) {
 		let sum = 0;
 		flavours.forEach((item) => {
@@ -16,18 +19,18 @@
 		});
 		return sum;
 	}
-
+	//计算用完的日期
 	function addDays(date, days) {
 		let result = new Date(date);
 		result.setDate(result.getDate() + days);
 		return result.toJSON().toString();
 	}
-
 </script>
 
 <body class="bg-gray-100">
 	<div class="absolute inset-48 leading-8 opacity-95">
 		<h2 class="text-2xl text-center"><ink data-type="blurred">润滑液</ink>计算器</h2>
+<!--		动态获取爱液信息-->
 		{#each aiye as flavour}
 			<label class="flex items-center mb-3 space-x-3">
 				<input
@@ -41,29 +44,28 @@
 			</label>
 		{/each}
 		<p>添加自定义mL</p>
-		<input
-				placeholder="添加自定义mL"
-				bind:value={customize}
-		><br>
+		<input placeholder="添加自定义mL" bind:value={customize} /><br />
+
 		<input type="range" bind:value={usage} min="1" max="20" />
 		单次用量：<span>{usage}mL</span><br />
 		<input type="range" bind:value={frequency} min="0.25" max="5.0" step="0.25" />
 		频率：一天<span>{frequency}</span>次<br />
 		购入日期：
 		<input type="date" bind:value={purchaseDate} />
-
-		{#if flavours <= 0 & customize === 0 | customize === null}
+<!--		如果没有进行输入则显示prompt，否则显示结果-->
+		{#if ((flavours <= 0) & (customize === 0)) | (customize === null)}
 			<p>选择你目前拥有的<ink data-type="blurred"> 润滑液 </ink></p>
 		{:else}
 			<p>
-				目前总量：{parseInt(join(flavours))+parseInt(customize)} mL<br />
-				预计在冲 {Math.ceil((parseInt(join(flavours))+parseInt(customize)) / usage)} 次后耗尽<br />
+				目前总量：{parseInt(join(flavours)) + parseInt(customize)} mL<br />
+				预计在冲 {Math.ceil((parseInt(join(flavours)) + parseInt(customize)) / usage)} 次后耗尽<br
+				/>
 				{#if purchaseDate != null}
 					耗尽日期：<span
-						>{addDays(purchaseDate, Math.ceil((parseInt(join(flavours))+parseInt(customize)) / usage) / frequency).slice(
-							0,
-							10
-						)}</span
+						>{addDays(
+							purchaseDate,
+							Math.ceil((parseInt(join(flavours)) + parseInt(customize)) / usage) / frequency
+						).slice(0, 10)}</span
 					>
 				{/if}
 			</p>
@@ -72,7 +74,6 @@
 </body>
 
 <style>
-	h1,
 	h2,
 	p,
 	label {
